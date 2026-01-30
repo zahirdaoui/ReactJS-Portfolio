0b0a9f4db13369900dc31bb3476c3cf0cb9be233
zahirdaoui <ezzahiredaoui@gmail.com>
2026-01-30 12:29:29 +0100

    add all

--- src/data/project.js
+++ src/data/project.js
@@ -38,11 +38,11 @@ export const projectData = [
   {
     "id":2,
     "title":"Personal Blog Platform",
-    "description":"is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+    "description":"Built a full-stack blog application with dual interfaces: a responsive visitor-facing front end for reading and interacting with content, and an admin dashboard for content management, moderation, and site administration.",
     "cover_alt":"is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-    "cover":"website-cover.avif",
-    "tags":["Bootstrap","Php","Mysql"],
-    "link":"https://web.ezzahir.com/",
+    "cover":"myblog.png",
+    "tags":["Php","MVC","Mysql","Bootstrap"],
+    "link":"https://blog.ezzahir.com/",
 
     "informations":[
             {
